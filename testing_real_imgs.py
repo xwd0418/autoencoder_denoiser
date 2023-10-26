fixed kl-div bug

testing_real_imgs.py
@@ -75,6 +75,8 @@ criterion = torch.nn.MSELoss(reduction="sum")
 def test(*model_tests):
     need_display = True
     for model_test in model_tests:
+        testing_result_dir = f'/root/autoencoder_denoiser/testing_real_imgs_results/{model_test.name}/'
+        os.makedirs(testing_result_dir, exist_ok= True)   
         displayed=0
         display_num = 0
         loss = 0
@@ -82,7 +84,8 @@ def test(*model_tests):
         plt.rcParams["figure.figsize"] = (20,10)
         with torch.no_grad():
             for iter, data in enumerate(tqdm(test_loader)):
-                noise, raw = data
+                # print(data)
+                noise, raw, name = data
                 if len(raw.shape)==3:   
                     raw, noise = raw.unsqueeze(1), noise.unsqueeze(1)
                 raw, noise = raw.to(device).float(), noise.to(device).float()
@@ -102,9 +105,11 @@ def test(*model_tests):
                 orig_SNR, denoised_SNR, SNR_incr = [sum(x) for x in zip( (orig_SNR, denoised_SNR, SNR_incr) , \
                                                                             compute_metrics(raw, noise, prediction))] 
             
-                if need_display and displayed<2:
-                    noise_pic , prediction_pic, raw_pic = noise[1],prediction[1], raw[1]                  
-                    display_pics(noise_pic[0].cpu(), prediction_pic[0].cpu(), raw_pic[0].cpu())
+                if need_display and displayed<5:
+                    noise_pic , prediction_pic, raw_pic = noise[0],prediction[0], raw[0]          
+                    save_path = testing_result_dir+f"{iter}.png"
+                    print(save_path)
+                    display_pics(noise_pic[0].cpu(), prediction_pic[0].cpu(), raw_pic[0].cpu(),save_path=save_path)
                     displayed = displayed+1
             
                     
@@ -174,9 +179,7 @@ if __name__ == "__main__":
     
     # dann_test.config['dataset']['real_img_keep_size'] = True 
 
-    test_thresholding(test_loader, threshold_value = 0.9 , dir_name_to_save="theshold_9_all")
+    # test_thresholding(test_loader, threshold_value = 0.9 , dir_name_to_save="theshold_9_all")
     
-    # for iter, data in enumerate(test_loader):
-    #     noise, raw,name = data
-    #     print(iter)
-    #     # print(noise.shape)
+    my_model_test = Test("paper_1d")
+    test(my_model_test)

transfer_w_cross_validation.py
@@ -33,7 +33,10 @@ class CrossValidateDataset(Dataset):
 
     def __getitem__(self, index):
         loaded_data = np.load(self.paths[index])
-        return np.expand_dims(loaded_data['noise'], 0),  np.expand_dims(loaded_data['ground_truth'], 0)
+        noise, ground_truth = loaded_data['noise'], loaded_data['ground_truth']
+        noise = cv2.resize(noise, (120, 180))
+        ground_truth = cv2.resize(ground_truth, (120, 180))
+        return np.expand_dims(noise, 0),  np.expand_dims(ground_truth, 0)
 
 
 class DenoiseExp(object):
@@ -44,6 +47,7 @@ class DenoiseExp(object):
         self.config = config
 
         self.__experiment_dir = '/root/autoencoder_denoiser/exps/cross_validation/'+name
+        os.system(f'rm -r {self.__experiment_dir}')
         self.device = torch.device("cuda:0")
         self.__criterion = torch.nn.MSELoss()
         self.__criterion = self.__criterion.cuda()
@@ -81,12 +85,16 @@ class DenoiseExp(object):
             test_partition = dataset[nighty_percent:]
             batch_size = self.config['dataset']['batch_size']
             print("training data amount: ", len(train_partition))
-            train_loader = DataLoader(CrossValidateDataset(
-                train_partition), batch_size=batch_size, shuffle=True)
-            val_loader = DataLoader(CrossValidateDataset(
-                val_partition), batch_size=batch_size, shuffle=True)
-            test_loader = DataLoader(CrossValidateDataset(
-                test_partition), batch_size=batch_size, shuffle=True)
+            train_loader = DataLoader(CrossValidateDataset( train_partition), batch_size=batch_size, shuffle=True,
+                                      persistent_workers=True,  num_workers=16, pin_memory = True
+                                      )
+            print('finishing preparing train loader')
+            val_loader = DataLoader(CrossValidateDataset(val_partition), batch_size=batch_size, shuffle=True,
+                                    persistent_workers=True,  num_workers=16, pin_memory = True
+                                      )
+            test_loader = DataLoader(CrossValidateDataset(test_partition), batch_size=batch_size, shuffle=True,
+                                     persistent_workers=True,  num_workers=16, pin_memory = True
+                                      )
             yield train_loader, val_loader, test_loader
             split_point = len(dataset)//k
             dataset = dataset[split_point:]+dataset[:split_point]
@@ -99,6 +107,7 @@ class DenoiseExp(object):
             # init model
             self.__model = get_model(self.config)
             self.__model = torch.nn.DataParallel(self.__model)
+            print('moving model to cuda')
             if torch.cuda.is_available():
                 self.__model = self.__model.cuda().float()
             self.load_model(self.config['loading_path'])
@@ -131,6 +140,7 @@ class DenoiseExp(object):
 
                 # train
                 for iter, data in enumerate((train_loader)):
+                    # print(f"training {iter}")
                     noise, ground_truth = data
                     ground_truth, noise = ground_truth, noise
                     ground_truth, noise = ground_truth.to(
@@ -200,8 +210,8 @@ if len(sys.argv) > 1:
     name = sys.argv[1]
 else:
     # default name
-    name = "plain_cross_valid"
-
+    name = "??"
+os.system('nvidia-smi -L')
 seed = 3405
 np.random.seed(seed)
 random.seed(seed)